09ca41705d8c98e8fbbf04a051780901098187a8
Yasmim <ypereira@outlook.com>
2017-11-04 10:13:44 -0200

    alteracoes referente ao primeiro exercicio - salvando filme na sessao

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -67,7 +67,7 @@ public class SalaController {
 
 		ModelAndView view = new ModelAndView("sessao/lista");
 		view.addObject("sessoes", sessaoDao.buscaSessoesDaSala(sala));
-
+		view.addObject("sala", sala);
 		return view;
 	}
 
