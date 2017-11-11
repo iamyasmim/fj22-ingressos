73c3f5bc7174216501c59f7a54868d2090ab83c2
Yasmim <ypereira@outlook.com>
2017-11-11 17:09:20 -0200

    6.6 e 6.8 realizando checkout e implementando a compra

--- src/main/java/br/com/caelum/ingresso/controller/CompraController.java
+++ src/main/java/br/com/caelum/ingresso/controller/CompraController.java
@@ -1,12 +1,19 @@
 package br.com.caelum.ingresso.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.caelum.ingresso.dao.CompraDao;
 import br.com.caelum.ingresso.dao.LugarDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Carrinho;
+import br.com.caelum.ingresso.model.Cartao;
 import br.com.caelum.ingresso.model.form.CarrinhoForm;
 
 public class CompraController {
@@ -20,6 +27,9 @@ public class CompraController {
 	@Autowired
 	private Carrinho carrinho;
 	
+	@Autowired
+	private CompraDao compraDao;
+	
 	@PostMapping("/compra/ingressos")
 	public ModelAndView enviarParaPagamento(CarrinhoForm carrinhoForm){
 		ModelAndView modelAndView = new ModelAndView("redirect:/compra");
@@ -28,5 +38,23 @@ public class CompraController {
 		
 		return modelAndView;
 	}
-
+	@GetMapping("/compra")
+	public ModelAndView checkout(Cartao cartao){
+		ModelAndView modelAndView = new ModelAndView("compra/pagamento");
+		
+		modelAndView.addObject("carrinho", carrinho);
+		
+		return modelAndView;
+	}
+	@PostMapping("/compra/comprar")
+	@Transactional
+	public ModelAndView comprar(@Valid Cartao cartao, BindingResult result){
+		ModelAndView modelAndView = new ModelAndView("redirect:/");
+		if(cartao.isValido()){
+			compraDao.save(carrinho.toCompra());
+		}else{
+			result.rejectValue("vencimento", "Vencimento invalido");
+			return checkout(cartao);
+		}
+	}
 }
